f8e80cb0ec0fb4f062e1a03f43b03861ef948bb5
ArthurLevchenko <levchenkoarthur31@gmail.com>
2023-12-10 12:33:31 +0200

    carousel

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ $('document').ready(function(){
 		$('body').toggleClass('lock')
 	})
 });
+
+
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+	loop:true,
+		 margin:0,
+		 nav:true,
+		 autoplay:true,
+		 autoplayTimeout:2000,
+		 navText: [ '', ' ' ],
+
+		 responsive:{
+			  0:{
+					items:1
+			  },
+			  1100:{
+					items:5
+			  },
+		 }
+	});
+});
